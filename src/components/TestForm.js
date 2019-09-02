c7b6ab78a386979b41fa8ed41d8517f752797c92
Artur Janas <artur.abato.janas@gmail.com>
2019-09-02 13:23:44 +0100

    adds local test field to the form

--- src/components/TestForm.js
+++ src/components/TestForm.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 const encode = (data) => {
   return Object.keys(data)
@@ -9,16 +9,17 @@ const encode = (data) => {
 export default class TestForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { name: "", email: "", message: "" };
+    this.state = {form:{ name: "", email: "", message: "", local:"" }}
   }
 
   /* Here’s the juicy bit for posting the form submission */
 
   handleSubmit = e => {
-    fetch("/", {
+    // console.log('try submit')
+    fetch("https://zen-hamilton-2fec8a.netlify.com/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contact", ...this.state })
+      body: encode({ "form-name": "contact", ...this.state.form })
     })
       .then(() => alert("Success!"))
       .catch(error => alert(error));
@@ -26,31 +27,53 @@ export default class TestForm extends React.Component {
     e.preventDefault();
   };
 
-  handleChange = e => this.setState({ [e.target.name]: e.target.value });
+  handleChange = e => this.setState({ form: {[e.target.name]: e.target.value }});
+
+  nextPage = e => {
+    this.page += 1
+    console.log(this.page)
+  }
 
   render() {
-    const { name, email, message } = this.state;
+    const { name, email, message, local } = this.state
+    
     return (
-      <form onSubmit={this.handleSubmit}>
-        <p>
-          <label>
-            Your Name: <input type="text" name="name" value={name} onChange={this.handleChange} />
-          </label>
-        </p>
-        <p>
-          <label>
-            Your Email: <input type="email" name="email" value={email} onChange={this.handleChange} />
-          </label>
-        </p>
-        <p>
-          <label>
-            Message: <textarea name="message" value={message} onChange={this.handleChange} />
-          </label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>
+      <div>
+        
+
+    <form onSubmit={this.handleSubmit}>
+            <div>
+              <p>
+                <label>
+                  Local: <input type="text" name="local" value={local} onChange={this.handleChange} />
+                </label>
+              </p>
+              <p>
+                <label>
+                  Your Name: <input type="text" name="name" value={name} onChange={this.handleChange} />
+                </label>
+              </p>
+
+              <button onClick={this.nextPage}>save and next</button>
+            </div>
+            <div>
+              <p>
+                <label>
+                  Your Email: <input type="email" name="email" value={email} onChange={this.handleChange} />
+                </label>
+              </p>
+              <p>
+                <label>
+                  Message: <textarea name="message" value={message} onChange={this.handleChange} />
+                </label>
+              </p>
+            </div>
+          
+        </form>
+
+
+        <button onClick={this.handleSubmit}>Submit outside</button>
+      </div>
     );
   }
 }
